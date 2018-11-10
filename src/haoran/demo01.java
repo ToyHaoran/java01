b3f8b0a2764f61a8203639a3b412176105abe887
lihaoran <toyhaoran@gmail.com>
2018-11-10 16:17:15 +0800

    Initial commit

---
+++ src/haoran/demo01.java
@@ -0,0 +1,13 @@
+package haoran;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lihaoran
+ * Date: 2018/11/10
+ * Description:
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("练习文件夹");
+    }
+}
