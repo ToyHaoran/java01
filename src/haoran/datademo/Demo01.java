7f554cb8da83b9a0dce010e8ddad31f602608034
lihaoran <toyhaoran@gmail.com>
2018-11-21 17:51:16 +0800

    更新

---
+++ src/haoran/datademo/Demo01.java
@@ -0,0 +1,14 @@
+package haoran.datademo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lihaoran
+ * Date: 2018/11/21
+ * Time: 17:42
+ * Description:
+ */
+public class Demo01 {
+
+
+
+}
--- src/haoran/demo01.java
+++ src/haoran/demo01.java
@@ -10,4 +10,7 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("练习文件夹");
     }
+    public void ddd() {
+
+    }
 }
